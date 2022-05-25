952bbf8f3221c325526c7e4904365c0a52772554
Abby Nyakara <abbynyakara@gmail.com>
2022-05-25 11:03:49 +0300

    capstone project files

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-// Mobile Menu 
+// Mobile Menu
 const open = document.querySelector('.top-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const close = document.querySelector('.cancel');
 
 open.addEventListener('click', () => {
-    mobileMenu.classList.add('show');
-})
+  mobileMenu.classList.add('show');
+});
 
 close.addEventListener('click', () => {
-    mobileMenu.classList.remove('show');
-})
+  mobileMenu.classList.remove('show');
+});
 
 // Create Dynamic section
 
 const featured = document.querySelector('.featured');
 
 const featuredArray = [
-    {
-        profileImg: 'images/ph1.jpg',
-        personName: 'Ray Chen',
-        personTitle: 'Violinist - America',
-        description: 'Ray has played the violin since 3years.The former winner of the Queen Elizabeth Violin Competition will be perfoming the Mozart concerto',
-    },
+  {
+    profileImg: 'images/ph1.jpg',
+    personName: 'Ray Chen',
+    personTitle: 'Violinist - America',
+    description: 'Ray has played the violin since 3years.The former winner of the Queen Elizabeth Violin Competition will be perfoming the Mozart concerto',
+  },
 
-    {
-        profileImg: 'images/ph2.jpg',
-        personName: 'Jeniffer Cho',
-        personTitle: 'Pianist - Melbourne',
-        description: 'Her motto: Make the piano sing! Jennifer quite literally does that and she will be making it sing this November for sure!',
-    },
+  {
+    profileImg: 'images/ph2.jpg',
+    personName: 'Jeniffer Cho',
+    personTitle: 'Pianist - Melbourne',
+    description: 'Her motto: Make the piano sing! Jennifer quite literally does that and she will be making it sing this November for sure!',
+  },
 
-    {
-        profileImg: 'images/ph3.jpg',
-        personName: "William O'Haire",
-        personTitle: 'Conductor- Italy',
-        description: "He inherited the Leningrad Philharmonic Orchestra in 2015 from the Austrian conductor Fritz Stiedry, and was largely responsible for maintaining the Austro-German tradition through the fraught years of Stalin’s Terror.",
-    },
+  {
+    profileImg: 'images/ph3.jpg',
+    personName: "William O'Haire",
+    personTitle: 'Conductor- Italy',
+    description: 'He inherited the Leningrad Philharmonic Orchestra in 2015 from the Austrian conductor Fritz Stiedry, and was largely responsible for maintaining the Austro-German tradition through the fraught years of Stalins Terror.',
+  },
 
-    {
-        profileImg: 'images/ph4.jpg',
-        personName: 'Jeff Mathews',
-        personTitle: 'Cellist - America',
-        description: 'In his own words, Jeff says "I started playing Cello because it soundedlike my grandfather humming! Now i get to share my grandfathers humming with the world!',
-    }
+  {
+    profileImg: 'images/ph4.jpg',
+    personName: 'Jeff Mathews',
+    personTitle: 'Cellist - America',
+    description: 'In his own words, Jeff says "I started playing Cello because it soundedlike my grandfather humming! Now i get to share my grandfathers humming with the world!',
+  },
 ];
 
 featured.innerHTML = featuredArray.map((project) => {
