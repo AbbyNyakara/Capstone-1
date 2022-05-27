d22a96cd5ae1e910ef2c338701c3c86a9e7a2d7b
Abby Nyakara <abbynyakara@gmail.com>
2022-05-27 15:10:49 +0300

    project-corrections

--- app.js
+++ app.js
@@ -11,66 +11,16 @@ close.addEventListener('click', () => {
   mobileMenu.classList.remove('show');
 });
 
-// Create Dynamic section
+// Create more button
 
-const featured = document.querySelector('.featured');
+const displayButton = document.getElementById('display-button');
+const allFeatures = document.querySelectorAll('.featured-section .feature');
 
-const featuredArray = [
-  {
-    profileImg: 'images/ph1.jpg',
-    personName: 'Ray Chen',
-    personTitle: 'Violinist - America',
-    description: 'Ray has played the violin since 3years.The former winner of the Queen Elizabeth Violin Competition will be perfoming the Mozart concerto',
-  },
-
-  {
-    profileImg: 'images/ph2.jpg',
-    personName: 'Jeniffer Cho',
-    personTitle: 'Pianist - Melbourne',
-    description: 'Her motto: Make the piano sing! Jennifer quite literally does that and she will be making it sing this November for sure!',
-  },
-
-  {
-    profileImg: 'images/ph3.jpg',
-    personName: "William O'Haire",
-    personTitle: 'Conductor- Italy',
-    description: 'He inherited the Leningrad Philharmonic Orchestra in 2015 from the Austrian conductor Fritz Stiedry, and was largely responsible for maintaining the Austro-German tradition through the fraught years of Stalins Terror.',
-  },
-
-  {
-    profileImg: 'images/ph4.jpg',
-    personName: 'Jeff Mathews',
-    personTitle: 'Cellist - America',
-    description: 'In his own words, Jeff says "I started playing Cello because it soundedlike my grandfather humming! Now i get to share my grandfathers humming with the world!',
-  },
-];
-
-// featured.innerHTML = featuredArray.map((project) => {
-//   const html = `
-//         <div class="feature">
-//                     <div class="left ray">
-//                         <img src="${project.profileImg}" alt="">
-//                     </div>
-//                     <div class="right">
-//                         <h3 class="artist-name">${project.personName}</h3>
-//                         <small class="artist-title">${project.personTitle}</small>
-//                         <p class="artist-description">${project.description}</p>
-//                     </div>
-//         </div>
-//     `;
-//   return html;
-// });
-
-if (screen.width <= 768){
-  const displayButton = document.getElementById('display-button');
-  const allFeatures = document.querySelectorAll('.featured-section .feature')
-
-  displayButton.addEventListener('click', ()=>{
-    // alert("hello");
-    allFeatures.forEach((oneFeature)=>{
-      oneFeature.classList.add('load-all');
-      oneFeature.classList.remove('onload-display');
-      displayButton.innerHTML = 'End of display';
-    });
+displayButton.addEventListener('click', () => {
+  // alert("hello");
+  allFeatures.forEach((oneFeature) => {
+    oneFeature.classList.add('load-all');
+    oneFeature.classList.remove('onload-display');
+    displayButton.innerHTML = 'End of display';
   });
-};
+});
